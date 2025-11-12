4ad13447e84ba975eb9252b57be8d751d42e5d6f
hassaanfarooq000 <muhammad.hassaan@nayatel.com>
2025-11-12 11:28:41 +0500

    updated CSP

--- next.config.ts
+++ next.config.ts
@@ -12,11 +12,11 @@ const nextConfig: NextConfig = {
             key: "Content-Security-Policy",
             value: [
               "default-src 'self'",
-              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.youtube.com https://www.youtube-nocookie.com",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.youtube.com https://www.youtube-nocookie.com https://www.googletagmanager.com",
               "style-src 'self' 'unsafe-inline'",
               "img-src 'self' data: https: https://i.ytimg.com",
               "font-src 'self' data:",
-              "connect-src 'self'",
+              "connect-src 'self' https://www.google-analytics.com https://www.googletagmanager.com",
               "frame-src https://www.youtube.com https://www.youtube-nocookie.com",
               "child-src https://www.youtube.com https://www.youtube-nocookie.com",
               "media-src 'self' https:",
