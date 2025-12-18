ffa1e933e8851e14af6e15b6e96a21a8e6d42f5f
hassaanfarooq000 <muhammad.hassaan@nayatel.com>
2025-12-18 11:25:18 +0500

    Update layout.tsx to enforce dynamic rendering and set revalidation to zero for improved data freshness.

--- app/layout.tsx
+++ app/layout.tsx
@@ -76,6 +76,8 @@ export const metadata: Metadata = {
     shortcut: "/favicon.ico",
   },
 };
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
 
 export default function RootLayout({
   children,
